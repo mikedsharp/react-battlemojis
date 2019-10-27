5d0ca6e085a4d93e514e36883a0bd7e46b430ffe
programmerinprogress <msharp23@hotmail.co.uk>
2019-10-27 19:05:36 +0000

    Still working the typing out

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Square } from '../Square/Square';
 import './Board.scss';
-export function Board(props: any) {
+export interface BoardProps {
+  player: any;
+}
+export function Board(props: BoardProps) {
   const renderSquares = () => {
     return props.player.squares.map(() => {
       return <Square />;
--- src/Square/Square.tsx
+++ src/Square/Square.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './Square.scss';
 
-export class SquareProps {
-  foo: string = '';
-}
-export const Square: React.FunctionComponent = () => {
+export interface SquareProps {}
+export const Square: React.FunctionComponent<SquareProps> = () => {
   return (
     <div className="Square">
       <span>🤪</span>
